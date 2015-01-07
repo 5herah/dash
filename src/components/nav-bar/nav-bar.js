867970dabbe3f49dcad16ca9e717456993e6ef92
Sherah Smith <ssmith@wikimedia.org>
2015-01-06 17:08:23 -0800

    sidebar nav changes
    Change-Id: I15a781afdf5ecf2ccfbaf167d9f39db1b3966ab2

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -9,6 +9,14 @@ define( [
         self.loggedIn = params.loggedIn;
         self.welcome = params.welcome;
 
+        //for now make this dummy - it should come from params/global user settings
+        self.userBoards = ko.observableArray([
+            { name: "Big English" },
+            { name: "A/B Testing" },
+            { name: "Fraud Monitoring" },
+            { name: "Times Honey is Cute" },
+        ]);
+
         self.hideNav = function(){
         	//make the nav menu fold out of view.
             $('#navContainer .navWrapper').toggleClass('hide');
@@ -22,6 +30,16 @@ define( [
                 $('#dashApp').css('padding-left', '175px');
             }, 200);
         };
+
+        self.selectNav = function(d, e){
+            $('.mainNavButton').removeClass('selectedSubNav');
+            $(e.target).addClass('selectedSubNav');
+        };
+
+        self.toggleBoardList = function(){
+            $('.subNavBoardOpts').toggleClass('hide');
+        };
+
     }
 
     return { viewModel: NavBarViewModel, template: template };
